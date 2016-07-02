c7466d6e8a36d463731e896bb0414f0098d328c3
Reza_Taheri <art.ntc4@gmail.com>
2016-07-02 21:46:34 +0430

    Parallel Computing

--- monte_carlo.m
+++ monte_carlo.m
@@ -1,6 +1,6 @@
 function F12 = monte_carlo(r1,r2,L,total)
 z = zeros(1,total); %set to number of parameters
-for i=1:length(z)
+parfor i=1:length(z)
 %% processing
 z(1,i) = f_line(r1,r2,L);
 %% end of processing
