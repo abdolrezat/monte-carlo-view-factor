29ae77bdafc3328839f6562ad5b6825e673a5521
Reza_Taheri <art.ntc4@gmail.com>
2016-07-02 21:48:31 +0430

    Parallel Simulation

--- Simulator_.m
+++ Simulator_.m
@@ -4,12 +4,11 @@ r2_L = 1.25;
 j = 1;
 rangeL_r1 = 0:0.1:5;
 total = 2000000;
-for L_r1 = rangeL_r1
+parfor j = 1:length(rangeL_r1)
     r2 = r2_L*L;
     r1 = L/L_r1;
     MCarlo(j) = monte_carlo(r1,r2,L);
     Analytic(j) = analytical_values(r1,r2,L,total);
-    j = j + 1;
 end
 
 plot(rangeL_r1,MCarlo,'b.');
