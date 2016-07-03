0374f5bc542f7ef4073d061fcc9aabd941de2d34
Reza_Taheri <art.ntc4@gmail.com>
2016-07-03 12:25:43 +0430

    fixes and removed preallocating since it does not improve speed

--- Simulator_single.m
+++ Simulator_single.m
@@ -1,6 +1,6 @@
 function [MCarlo,Analytic] = Simulator_single(rangeL_r1,r2_L,L,total)
-MCarlo = zeros(length(rangeL_r1));
-Analytic = zeros(length(rangeL_r1));
+% MCarlo = zeros(1,length(rangeL_r1));
+% Analytic = zeros(1,length(rangeL_r1));
 for j = 1:length(rangeL_r1)
     r2 = r2_L*L;
     r1 = L/rangeL_r1(j);
