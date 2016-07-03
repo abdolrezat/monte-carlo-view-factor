357241b45c3b4a7cf9715745aae7257679ac1a72
Reza_Taheri <art.ntc4@gmail.com>
2016-07-03 12:19:03 +0430

    Preallocation

--- Simulator_single.m
+++ Simulator_single.m
@@ -1,6 +1,7 @@
 function [MCarlo,Analytic] = Simulator_single(rangeL_r1,r2_L,L,total)
-
-parfor j = 1:length(rangeL_r1)
+MCarlo = zeros(length(rangeL_r1));
+Analytic = zeros(length(rangeL_r1));
+for j = 1:length(rangeL_r1)
     r2 = r2_L*L;
     r1 = L/rangeL_r1(j);
     MCarlo(j) = monte_carlo(r1,r2,L,total);
