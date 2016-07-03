e39cf24220923700c7d3e59af607197743a82578
Reza_Taheri <art.ntc4@gmail.com>
2016-07-03 12:30:29 +0430

    final

--- Simulator_single.m
+++ Simulator_single.m
@@ -1,7 +1,7 @@
 function [MCarlo,Analytic] = Simulator_single(rangeL_r1,r2_L,L,total)
 % MCarlo = zeros(1,length(rangeL_r1));
 % Analytic = zeros(1,length(rangeL_r1));
-for j = 1:length(rangeL_r1)
+parfor j = 1:length(rangeL_r1)
     r2 = r2_L*L;
     r1 = L/rangeL_r1(j);
     MCarlo(j) = monte_carlo(r1,r2,L,total);
