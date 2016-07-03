f2dd7758667d526963126dc70badacece150a3f2
Reza_Taheri <art.ntc4@gmail.com>
2016-07-03 08:33:05 +0430

    Release Version

--- Simulator_Multi.m
+++ Simulator_Multi.m
@@ -1,17 +1,19 @@
 tic
 %
 L = 1;
-rangeL_r1 = 0:0.1:10;
-total = 100000;
+rangeL_r1 = 0:0.01:10;
+total = 5000000;
 r2_L = [0.3,0.4,0.6,0.8,1.0,1.25,1.5,2,3,4,5,6,8];
 parfor i = 1:length(r2_L)
     [MCarlo,Analytic] = Simulator_single(rangeL_r1,r2_L(i),L,total);
     CORE(:,i) = {MCarlo,Analytic};
 end
 hold on;
-for(i = 1:length(r2_L))
+for i = 1:length(r2_L)
     plot(rangeL_r1,CORE{1,i},'b.')
     plot(rangeL_r1,CORE{2,i},'r')
 end
 %
 toc
+% load handel
+% sound(y(1:16500),Fs)
